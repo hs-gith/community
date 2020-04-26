a4df281ef3aa658b26a67c74be8aa356493c5816
shan <1822124470@qq.com>
2020-04-26 13:48:29 +0800

    111

--- src/main/java/com/hs/controller/Test.java
+++ src/main/java/com/hs/controller/Test.java
@@ -1,7 +1,10 @@
 package com.hs.controller;
 
+
+import java.time.LocalDateTime;
+
 public class Test {
     public static void main(String[] args) {
-
+        System.out.println(LocalDateTime.now());
     }
 }
